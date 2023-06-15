0c1d4c97de8125e8b7d7bba63e82014638fa2898
Sam4 <134475159+Sam-Hor@users.noreply.github.com>
2023-06-15 23:41:21 +0400

    first scrapping in cyrillic

--- main_01.py
+++ main_01.py
@@ -1,4 +1,13 @@
-
+import requests
 from bs4 import BeautifulSoup
-with open("index2.html") as file:
-    src = file.read()
+
+
+url = 'https://journal.tinkoff.ru/pro/'
+r = requests.get(url)
+r.encoding = 'utf-8'
+# print(r.text)
+soup = BeautifulSoup(r.text, 'lxml')
+# href обязательный атрибут для ссылок
+a = soup.find('div', class_='card--P0w4a').find('div',
+                                                class_='title--xDW1O').find('span')
+print(a.string)
