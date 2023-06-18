b1752b093df2d4585ae0193b3317d8293266549b
Sam4 <134475159+Sam-Hor@users.noreply.github.com>
2023-06-18 20:45:50 +0400

    add pages search + create csv file output

--- main_01.py
+++ main_01.py
@@ -1,23 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
+from time import sleep
+import pandas as pd
 
-for page in range(1, 12):
+data = []
+for page in range(1, 13):
+    print(page)
     url = f'https://journal.tinkoff.ru/flows/sravnyator/best/page/{page}/'
-
     r = requests.get(url)
+    sleep(3)
     r.encoding = 'utf-8'
-    # print(r.text)
     soup = BeautifulSoup(r.text, 'lxml')
-# href обязательный атрибут для ссылок
 
-# titles = soup.find('div', class_='header--T3Cfh')
-# print(titles.text)
-# print()
-
-# ======задача вывести и лайки и заметки после каждой записи
-
-
-# 1 успех
     all_titles = soup.findAll('div', class_='header--T3Cfh')
     for i in all_titles:
         date = i.find('div', class_='nowrap--wLyr4').find('time').text
@@ -27,9 +21,14 @@ for page in range(1, 12):
         print(title, '//', author)
         link = i.find('a', class_='link--r6yOY').get('href')
         print('https://journal.tinkoff.ru'+link)
+                  
 
-# data.append([date, title, author, link])
+        data.append([date, title, author, link])
+header = ['date', 'title', 'author', 'link']
+df = pd.DataFrame(data, columns=header) 
+df.to_csv('/Users/Sam/Python_razr/website_parser_bot_01/parser.csv', sep=';', encoding='utf8')       
 
+          
 # 2
 # all_titles = soup.findAll('div', class_='item--HhAuM')
 # for i in all_titles:
